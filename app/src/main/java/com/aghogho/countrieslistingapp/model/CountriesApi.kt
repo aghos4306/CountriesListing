0fabfaa94a6df080f33beecab8574febf358869d
aghoghoworkpc <aghogho.akpedeye@gmail.com>
2022-05-13 18:56:36 +0300

    create retrofit service

---
+++ app/src/main/java/com/aghogho/countrieslistingapp/model/CountriesApi.kt
@@ -0,0 +1,11 @@
+package com.aghogho.countrieslistingapp.model
+
+import io.reactivex.Single
+import retrofit2.http.GET
+
+interface CountriesApi {
+
+    @GET("DevTides/countries/master/countriesV2.json")
+    fun getCountries(): Single<List<Country>>
+
+}
