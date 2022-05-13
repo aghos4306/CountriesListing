b87ccaa38ebf66808f64763afd47264455f63700
aghoghoworkpc <aghogho.akpedeye@gmail.com>
2022-05-13 13:39:17 +0300

    create the structure of the viewmodel

--- app/src/main/java/com/aghogho/countrieslistingapp/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/aghogho/countrieslistingapp/viewmodel/ListViewModel.kt
@@ -1,7 +1,36 @@
 package com.aghogho.countrieslistingapp.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.aghogho.countrieslistingapp.model.Country
 
 class ListViewModel: ViewModel() {
+
+    val countries = MutableLiveData<List<Country>>()
+    val countryLoadError = MutableLiveData<Boolean>()
+    val loading = MutableLiveData<Boolean>()
+
+    fun refresh() {
+        fetchCountries()
+    }
+
+    private fun fetchCountries() {
+        val mockData = listOf(
+            Country("Denmark"),
+            Country("Finland"),
+            Country("United Kingdom"),
+            Country("United Arab Emirates"),
+            Country("Germany"),
+            Country("Israel"),
+            Country("Netherland"),
+            Country("Spain"),
+            Country("Italy"),
+            Country("Luzemburgh")
+        )
+
+        countryLoadError.value = false
+        loading.value = false
+        countries.value = mockData
+    }
 
 }
