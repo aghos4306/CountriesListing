0b66bf1a0b1272b2fcd95392077301032e95cf1f
aghoghoworkpc <aghogho.akpedeye@gmail.com>
2022-05-13 18:36:57 +0300

    implement view in MainActivity using viewmodel and adapter declaration and view binding

--- app/src/main/java/com/aghogho/countrieslistingapp/view/MainActivity.kt
+++ app/src/main/java/com/aghogho/countrieslistingapp/view/MainActivity.kt
@@ -1,12 +1,56 @@
 package com.aghogho.countrieslistingapp.view
 
+
+import android.arch.lifecycle.ViewModelProviders
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.aghogho.countrieslistingapp.R
+import com.aghogho.countrieslistingapp.databinding.ActivityMainBinding
+import com.aghogho.countrieslistingapp.viewmodel.ListViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    lateinit var viewModel: ListViewModel
+    private val countriesAdapter = CountryListAdapter(arrayListOf())
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        //setContentView(R.layout.activity_main)
+        viewModel = ViewModelProvider(this).get(ListViewModel::class.java)
+        viewModel.refresh()
+        binding.countriesList.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = countriesAdapter
+        }
+
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+        viewModel.countries.observe(this, Observer { countries ->
+            countries?.let { countriesAdapter.updateCountries(it) }
+        })
+
+        viewModel.countryLoadError.observe(this, Observer { isError ->
+            isError?.let {
+                binding.listError.visibility = if (it) View.VISIBLE else View.GONE}
+        })
+
+        viewModel.loading.observe(this, Observer { isLoading ->
+            isLoading?.let {
+                binding.loadingView.visibility = if (it) View.VISIBLE else View.GONE
+                if (it) {
+                    binding.listError.visibility = View.GONE
+                    binding.countriesList.visibility = View.GONE
+                }
+            }
+        })
     }
 }
